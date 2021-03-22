ea9141da4743fbc399f1ca6873f9daf5cd80db61
Blaxion <khibaziev.f@gmail.com>
2021-03-22 19:39:25 +0100

    Initial commit

---
+++ JSS/Exo1/Public/js/main.js
@@ -0,0 +1,48 @@
+// let monNom = "Khibaziev";
+// let monPrenom = "Farhad";
+// let monAge = 28;
+// let maVille= "Bruxelles";
+
+// console.log ("je m'apelle " +monPrenom+" "+monNom+" j'ai "+monAge+" ans et j'habite à "+maVille);
+
+// let x = 10;
+// let y = 5;
+
+// let monAddition = x+y;
+// let maMultiplication = x*y;
+// let maSoustraction = x-y;
+// let maDivision = x/y;
+
+// console.log (`${monAddition} ${maMultiplication} ${maSoustraction} ${maDivision}`);
+
+// console.log (monAddition +" "+ maMultiplication +" "+ maSoustraction + " " +maDivision);
+
+
+// let plat = prompt ("Qu'as tu mangé ce soir");
+// q2 = prompt (`As tu aimés manger ${plat}
+// let chocolat = prompt("Aimes-tu le chocolat, oui ou non ?");
+// if (chocolat == "oui"){
+//     alert("Bravo tu aimes le chocolat")
+// } else if ( chocolat == "non"){
+//     alert("c'est étrange çà")
+// } else 
+//     alert("Votre réponse n'a pas de sens");
+
+
+let verreEau = "Eau"
+let verreCoca = "Coca"
+let verreThe = "The"
+let verreOrange = "Orange"
+
+
+verreVide = verreEau
+verreEau = verreCoca
+verreCoca = verreVide
+
+verreVide = verreThe
+verreThe = verreOrange
+verreOrange =verreVide
+
+console.log(`${verreEau} ${verreCoca} ${verreThe} ${verreOrange}`);
+
+ 
